b1d759a9995753e971f4a95bac9d38647f48acde
Abdulrahman <d7mii.b@gmail.com>
2022-05-24 10:46:52 +0300

    make navbar response

--- front-end/src/js/student/NavbarSt.js
+++ front-end/src/js/student/NavbarSt.js
@@ -7,29 +7,36 @@ const NavbarSt = () => {
     return (
         <nav className="navbar">
 
-            <ul className='list'>
-                <li><Link to='/home'>الرئيسية</Link></li>
+            <div className='list-section'>
+                <div className='list-bar'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM64 256C64 238.3 78.33 224 96 224H480C497.7 224 512 238.3 512 256C512 273.7 497.7 288 480 288H96C78.33 288 64 273.7 64 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"/></svg>
+                </div>
 
-                {/* SERVICES LIST */}
-                <li className='services'>
-                    <header>الخدمات </header>
-                    <ul className='services-list nav-list'>
-                        <li><Link to='/renew'>تجديد القيد</Link></li>
-                        <li><Link to='/degree-statement'>بيان درجات</Link></li>
-                        <li><Link to='#'>شهادة القيد</Link></li>
-                        <li><Link to='#'>رفع تظلم</Link></li>
-                    </ul>
-                </li>
+                <ul className='list'>
+                    <li><Link to='/home'>الرئيسية</Link></li>
 
-                {/* SCHEDULES LIST */}
-                <li className='schedules'>
-                    <header>الجداول</header>
-                    <ul className='schedules-list nav-list'>
-                        <li><Link to='/schedule'>جدول المحاضرات</Link></li>
-                        <li><Link to='/calender'>التقويم الجامعي</Link></li>
-                    </ul>
-                </li>
-            </ul>
+                    {/* SERVICES LIST */}
+                    <li className='services'>
+                        <header>الخدمات </header>
+                        <ul className='services-list nav-list'>
+                            <li><Link to='/renew'>تجديد القيد</Link></li>
+                            <li><Link to='/degree-statement'>بيان درجات</Link></li>
+                            <li><Link to='#'>شهادة القيد</Link></li>
+                            <li><Link to='#'>رفع تظلم</Link></li>
+                        </ul>
+                    </li>
+
+                    {/* SCHEDULES LIST */}
+                    <li className='schedules'>
+                        <header>الجداول</header>
+                        <ul className='schedules-list nav-list'>
+                            <li><Link to='/schedule'>جدول المحاضرات</Link></li>
+                            <li><Link to='/calender'>التقويم الجامعي</Link></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
             <section className='icons'>
                 <img src={logo} alt='HUG' />
 
@@ -45,6 +52,12 @@ const NavbarSt = () => {
                             <p>هندسة حاسوب</p>
                             <p>مستوى رابع</p>
                         </section>
+                        <div className='wallet wallet-pro'>
+                            <svg className='wallet-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M448 32C465.7 32 480 46.33 480 64C480 81.67 465.7 96 448 96H80C71.16 96 64 103.2 64 112C64 120.8 71.16 128 80 128H448C483.3 128 512 156.7 512 192V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H448zM416 336C433.7 336 448 321.7 448 304C448 286.3 433.7 272 416 272C398.3 272 384 286.3 384 304C384 321.7 398.3 336 416 336z"/></svg>
+                            <div className='wallet-balance'>
+                                : 2000
+                            </div>
+                        </div>
                         <Link className='btn' to='/'>تسجيل الخروج</Link>
                     </div>
                 </div>
