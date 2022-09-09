3a494acf8caebca964caf916f7710174279dabb8
hlool24 <halahhelabi@gmail.com>
2022-09-09 18:51:36 +0300

    Add editEmployee interface

--- react-node-app/client/src/js/admin/editEmployees.js
+++ react-node-app/client/src/js/admin/editEmployees.js
@@ -7,7 +7,7 @@ const EditEmployee = () => {
     
 
     
-    const handelSubmit = async (e) => {
+    const handelSubmit =  (e) => {
         e.preventDefault();
 
         const form = document.getElementById("chargeWallet-form");
@@ -17,6 +17,7 @@ const EditEmployee = () => {
         const amount = +form.amount.value;
         const date = form.date.value;
         
+    }
 
         
 
@@ -38,13 +39,39 @@ const EditEmployee = () => {
                 <input name="password" type='password' required />
                 <label htmlFor="vali">تأكيد الرقم السري</label>
                 <input name="date" type='password' required />
+                <div className='chechbox'>
+                    <div className='entercheckbox'>
+                <input type="checkbox" name="news" value="news" className='check' />
+                <label for="news">إضافة اخبار</label>
+                </div>
+                <div className='entercheckbox'>
+                <input type="checkbox" name="advertisements" value="advertisements" className='check' />
+                <label for="advertisements">إضافة اعلانات</label>
+                </div>
+                <div className='entercheckbox'>
+                <input type="checkbox" name="chargeWallet" value="chargeWallet" className='check' />
+                <label for="chargeWallet">شحن محفظة الطالب </label> 
+                </div>
+                <div className='entercheckbox'>
+                <input type="checkbox" name="editSchedule" value="editSchedule" className='check' />
+                <label for="editSchedule">اضافة الجداول </label>
+                </div>
+                <div className='entercheckbox'>
+                <input type="checkbox" name="griverence" value="griverence" className='check' />
+                <label for="griverence">مراجعة التظلمات </label>
+                </div>
+                <div className='entercheckbox'>
+                <input type="checkbox" name="suggestions" value="suggestions" className='check' />
+                <label for="suggestions">مراجعة الشكاوي والاقتراحات </label> 
+                </div>               
+                </div>
                 <section className='buttons'>
                     <button className='btn'>إضافة</button>
                 </section>
             </form>
         </div>
     );
-}
+
 }
 
 export default EditEmployee;
